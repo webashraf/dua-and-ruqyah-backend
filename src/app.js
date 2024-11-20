5661e208a62b071b23568bf6e424a71661bd0376
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-11-21 00:43:10 +0600

    add get dua category and sub category

--- src/app.js
+++ src/app.js
@@ -1,14 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('../db');
 
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// Get all categories
+app.get('/categories', (req, res) => {
+    const query = 'SELECT * FROM category';
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows); // Send category data as JSON
+    });
+});
 
+// Get subcategories by category ID
+app.get('/sub-category/:id', (req, res) => {
+    const categoryId = req.params.id;
+    console.log(categoryId);
+    const query = 'SELECT * FROM sub_category WHERE cat_id = ?';
+    db.all(query, [categoryId], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows); // Send subcategory data as JSON
+    });
+});
+// Get duas by subcategory ID
+app.get('/dua/:id', (req, res) => {
+    const subcategoryId = req.params.id;
+    const query = 'SELECT * FROM dua WHERE subcat_id = ?';
+    db.all(query, [subcategoryId], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(rows); // Send dua data as JSON
+    });
+});
+// Default route
 app.get('/', (req, res) => {
     res.send('Dua Backend is running!');
 });
---
+++ src/server.js
@@ -0,0 +1,5 @@
+// import app from "./app";
+
+// app.listen(process.env.port, () => {
+//   console.log(`Server app listening on port ${process.env.port}`);
+// });
